36e763b244dc5b20b640f635b344058cac112cbe
eto1302 <martinangelov1302@gmail.com>
2018-12-23 15:14:45 +0200

    Added Register Functionality

---
+++ EduMan/Models/BindingModels/RegisterBindingModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eduman.Models.BindingModels
+{
+    public class RegisterBindingModel
+    {
+
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 0)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Number")]
+        public int Number { get; set; }
+
+        [Required]
+        [Display(Name = "School")]
+        public string School { get; set; }
+
+        [Required]
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+    }
+}
