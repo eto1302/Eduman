a107af5c5fe9fd0f9af05f6f29e555cf81a71f8b
eto1302 <martinangelov1302@gmail.com>
2018-12-26 10:31:10 +0200

    Added Functionality for Displaying all Absences

---
+++ EduMan/Models/ViewModels/AllAbsencesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Eduman.Mapping;
+
+namespace Eduman.Models.ViewModels
+{
+    public class AllAbsencesViewModel
+    {
+        public string StudentName { get; set; }
+
+        public string TeacherName { get; set; }
+        
+        public string Subject { get; set; }
+
+        public DateTime DateAbsent { get; set; }
+        
+    }
+}
