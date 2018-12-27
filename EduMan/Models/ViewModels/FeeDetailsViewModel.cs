4ffa230d05f234b5ccbabd06e6ab746cdd174a93
eto1302 <martinangelov1302@gmail.com>
2018-12-27 20:50:46 +0200

    Added Fee Details Functionality

---
+++ EduMan/Models/ViewModels/FeeDetailsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eduman.Models.ViewModels
+{
+    public class FeeDetailsViewModel
+    {
+        public int Cost { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public string StudentFirstName { get; set; }
+
+        public string StudentLastName { get; set; }
+
+        public string TeacherFirstName { get; set; }
+
+        public string TeacherLastName { get; set; }
+
+        public string School { get; set; }
+    }
+}
