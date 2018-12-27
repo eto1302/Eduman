15b34b026b3b8794dc3e42c24914ab100d30ac5b
eto1302 <martinangelov1302@gmail.com>
2018-12-27 22:31:02 +0200

    Added Grades Listing Functionality

---
+++ EduMan/Models/BindingModels/CreateGradeBindingModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eduman.Models.BindingModels
+{
+    public class CreateGradeBindingModel
+    {
+        public string Subject { get; set; }
+
+        public string Description { get; set; }
+
+        public double Value { get; set; }
+
+        public string StudentFirstName { get; set; }
+
+        public string StudentLastName { get; set; }
+    }
+}
---
+++ EduMan/Models/ViewModels/AllGradesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eduman.Models.ViewModels
+{
+    public class AllGradesViewModel
+    {
+        public double Value { get; set; }
+
+        public string Id { get; set; }
+
+        public string Subject { get; set; }
+    }
+}
