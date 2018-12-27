c831b01b6c5c4125b246034b477334ac57884169
eto1302 <martinangelov1302@gmail.com>
2018-12-27 20:50:22 +0200

    Added All Fees Listing Functionality

---
+++ EduMan/Models/ViewModels/AllFeesViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eduman.Models.ViewModels
+{
+    public class AllFeesViewModel
+    {
+        public string Id { get; set; }
+        
+        public DateTime DueDate { get; set; }
+
+        public string StudentUsername { get; set; }
+
+        public string TeacherUsername { get; set; }
+        
+        public int Cost { get; set; }
+
+        
+    }
+}
