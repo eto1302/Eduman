3b177997d7fa4761d70bdde095b8a8723a678968
eto1302 <martinangelov1302@gmail.com>
2018-12-27 22:31:29 +0200

    Created Grade Details Functionality

---
+++ EduMan/Models/ViewModels/GradeDetailsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eduman.Models.ViewModels
+{
+    public class GradeDetailsViewModel
+    {
+        public string Subject { get; set; }
+
+        public string Description { get; set; }
+
+        public double Value { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public string StudentFirstName { get; set; }
+
+        public string StudentLastName { get; set; }
+
+        public string TeacherFirstName { get; set; }
+
+        public string TeacherLastName { get; set; }
+    }
+}
